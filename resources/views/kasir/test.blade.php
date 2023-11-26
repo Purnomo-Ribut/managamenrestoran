dad53f61acded53a68939c5c175f88ad8afa0184
Lutfi-77 <xenonoffic@gmail.com>
2023-11-27 03:32:17 +0700

    Starter kasir dashboard

---
+++ resources/views/kasir/test.blade.php
@@ -0,0 +1,20 @@
+@extends('kasir.master.template')
+
+@section('title', 'Dashboard kasir')
+
+@section('css')
+    {{-- <link rel="stylesheet" href=""> --}}
+@endsection
+    
+@section('content')
+    {{-- <div class="card">
+        <div class="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut maxime excepturi iusto voluptatum omnis. Velit, voluptatum. Molestiae non dolore illum libero velit, officia nihil consequuntur qui cum aspernatur nisi cumque.
+        </div>
+    </div> --}}
+@endsection
+
+{{-- Javascript --}}
+@push('scripts')
+
+@endpush
