2c6352f7fa69ecd4724a2005a75a55cad48246cb
Riko Andriawan <97824888+rikoandre@users.noreply.github.com>
2023-11-27 10:50:20 +0700

    Mencoba tambah Dash Manager
    Dash manager berisi bebrapa pilihan

---
+++ resources/views/manager/manager.blade.php
@@ -0,0 +1,106 @@
+@extends('kasir.master.template')
+
+@section('title', 'Dashboard Manager')
+
+@section('css')
+    {{-- <link rel="stylesheet" href=""> --}}
+@endsection
+    
+@section('content')
+    <!-- Main Content -->
+    <div class="container mt-4">
+        <h1>Welcome to Dashboard Manager</h1>
+        <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Data Menu</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalMenu">
+                    Menu
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Monitoring Data Karyawan</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Karyawan</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Monitoring Orderan</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Orderan</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Monitoring Data Chef</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Chef</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Monitoring Pendapatan</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">pendapatan</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Monitoring Pelanggan</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Pelanggan</a>
+                  </div>
+                </div>
+              </div>
+        </div>
+    </div>
+
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    Launch static backdrop modal
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Understood</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
+
+{{-- Javascript --}}
+@push('scripts')
+
+@endpush
