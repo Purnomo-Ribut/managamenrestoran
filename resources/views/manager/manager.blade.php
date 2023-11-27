0fa217e9affc7cfeeca41d21c694ac12a8b7b21e
kolekandre <kolekandre87@gmail.com>
2023-11-27 22:13:52 +0700

    membuat halaman manager dan form nya

--- resources/views/manager/manager.blade.php
+++ resources/views/manager/manager.blade.php
@@ -13,10 +13,13 @@
         <div class="col-sm-6">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Data Menu</h5>
+                    <h5 class="card-title">Monitpring Data Menu</h5>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalMenu">
-                        Menu
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalAddMenu">
+                        Tambah Menu
+                    </button>
+                    <button type="button" class="btn btn-primary">
+                        Daftar Menu
                     </button>
                 </div>
             </div>
@@ -24,20 +27,51 @@
         <div class="col-sm-6">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Monitoring Data Karyawan</h5>
+                    <h5 class="card-title">Monitoring Pelanggan</h5>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Karyawan</a>
+                    <a href="#" class="btn btn-primary">Pelanggan</a>
                 </div>
             </div>
         </div>
+        {{-- <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Monitoring Data Karyawan</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary"  data-toggle="modal" data-target="#modalAddMenu">Tambah Karyawan</a>
+                    <a href="#" class="btn btn-primary">Data Karyawan</a>
+                </div>
+            </div>
+        </div> --}}
     </div>
-    <div class="row">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Monitoring Orderan</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Lihat Orderan</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Monitoring Kategori</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#modalKategori">Tambah Kategori</a>
+                        <a href="#" class="btn btn-primary">Data Kategori</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
         <div class="col-sm-6">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Monitoring Orderan</h5>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Orderan</a>
+                    <a href="#" class="btn btn-primary">Lihat Orderan</a>
                 </div>
             </div>
         </div>
@@ -46,7 +80,8 @@
                 <div class="card-body">
                     <h5 class="card-title">Monitoring Data Chef</h5>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Chef</a>
+                    <a href="#" class="btn btn-primary">Tambah Chef</a>
+                    <a href="#" class="btn btn-primary">Data Chef</a>
                 </div>
             </div>
         </div>
@@ -61,42 +96,87 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Monitoring Pelanggan</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Pelanggan</a>
-                </div>
-            </div>
-        </div>
     </div>
-</div>
-
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-    Launch static backdrop modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Understood</button>
-            </div>
-        </div>
     </div>
-</div>
+
+  <!-- Modal add menu-->
+  <div class="modal fade" id="modalAddMenu" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama Menu</label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="contoh : Rendang">
+              </div>
+              <div class="mb-3">
+                <label for="harga" class="form-label">Harga</label>
+                <input type="number" class="form-control" id="harga" name="harga" placeholder="contoh : 50000">
+              </div>
+              <div class="mb-3">
+                <label for="id_kategori">Kategori</label>
+                <select class="form-control" name="id_kategori" id="id_kategori" required="required">
+
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </select>
+                    {{-- @foreach ($jurusans as $jurusan) --}}
+                    {{-- <option value="{{ $jurusan->id }}">{{ $jurusan->nama }}</option> --}}
+                    {{-- @endforeach --}}
+            </div>
+            <div class="mb-3">
+                <label for="stock" class="form-label">Stock</label>
+                <input type="number" class="form-control" id="stock" name="stock" placeholder="contoh : 20">
+              </div>
+              <div class="mb-3">
+                <label for="deskripsi" class="form-label">Deskripsi</label>
+                <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3" placeholder="Desripsi singkat menu tersebut"></textarea>
+              </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Modal add menu end-->
+
+  {{-- Modal add kategori --}}
+  <div class="modal fade" id="modalKategori" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama Kategori</label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="contoh : Makanan">
+              </div>
+              <div class="mb-3">
+                <label for="deskripsi" class="form-label">Deskripsi</label>
+                <textarea class="form-control" id="deskripsi" name="deskripsi" rows="3" placeholder="Desripsi singkat kategori tersebut"></textarea>
+              </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  {{-- Modal add kategori end--}}
 @endsection
 
 {{-- Javascript --}}
