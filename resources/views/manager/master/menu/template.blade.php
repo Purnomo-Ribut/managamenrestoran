070d3f6179a29841b7a63c38f5fb3ef4fc1aa921
kolekandre <kolekandre87@gmail.com>
2023-12-11 12:18:35 +0700

    update tampilan tipis-tipis

--- resources/views/manager/master/menu/template.blade.php
+++ resources/views/manager/master/menu/template.blade.php
@@ -75,12 +75,13 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard Manager</h1>
+            <h1 class="m-0">Dashboard data Menu</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"> <a href="{{ route('manager_index') }}" >Home</a></li>
               <li class="breadcrumb-item active">Dashboard</li>
+              <li class="breadcrumb-item active">Menu</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
