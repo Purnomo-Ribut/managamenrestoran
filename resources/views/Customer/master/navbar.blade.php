5baf10cb11e548988808674c90970c480cc6e6f6
key board <nemesislrim@gmail.com>
2023-12-13 20:01:49 +0700

    update tampilan

--- resources/views/Customer/master/navbar.blade.php
+++ resources/views/Customer/master/navbar.blade.php
@@ -1,5 +1,5 @@
 <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button"></a>
     </li>
   </ul>
