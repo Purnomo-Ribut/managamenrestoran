3cbb0f37fd4bd824ff2c24bcd8cd1aa96667d75b
key board <nemesislrim@gmail.com>
2023-12-14 13:11:58 +0700

    update-setelah-C0

--- routes/web.php
+++ routes/web.php
@@ -87,9 +87,9 @@ Route::middleware('registered')->group(function() {
     Route::get('/cart/remove/{id}','Customer\OrderController@removeCart')->name('remove.cart');
     Route::post('/cart/checkout','Customer\OrderController@checkout')->name('checkout.cart');
     // Route::get('/menu/minuman','Customer\MenuController@minuman')->name('minuman.index');
-    
+
     Route::post('/cart','Customer\OrderController@addCart')->name('addcart');
-    
+
     Route::get('/reservasi/flush','Customer\ReservationController@flush')->name('reservasi.logout');
 });
 
@@ -97,8 +97,9 @@ Route::middleware('registered')->group(function() {
 //Customer
 // Route::get('/menu/{id}','Customer\MenuController@index')->name('makanan.index');
 Route::get('/tes', function () {
-    return view('Customer.modal');
-});
+    return view('Customer.cekout');
+})->name('cekout');
+
 
 // CHEF
 
